ffc96e8f29da333a3a52d66293b2036acd18ab2d
Jason Maassen <jmaassen73@gmail.com>
2015-12-02 11:38:01 +0100

    Initial javadoc

--- src/ibis/constellation/ActivityIdentifier.java
+++ src/ibis/constellation/ActivityIdentifier.java
@@ -9,9 +9,9 @@ public class ActivityIdentifier implements Serializable {
     // public static final long UNKNOWN = Long.MAX_VALUE;
 
     // The globally unique UUID for this activity is "EID:AID"
-    // "EID" is the id of the executor on which this actvity was created,
+    // "CID" is the id of the Constellation on which this Activity was created,
     // "AID" is the sequence number of this activity on that executor.
-    public final long EID;
+    public final long CID;
     public final long AID;
     public final boolean expectsEvents;
 
@@ -21,7 +21,7 @@ public class ActivityIdentifier implements Serializable {
     // private long lastKnownEID;
 
     public ActivityIdentifier(long high, long low, boolean expectsEvents) {
-        this.EID = high;
+        this.CID = high;
         this.AID = low;
         this.expectsEvents = expectsEvents;
     }
@@ -43,14 +43,14 @@ public class ActivityIdentifier implements Serializable {
     }
 
     public ConstellationIdentifier getOrigin() {
-        return new ConstellationIdentifier(EID);
+        return new ConstellationIdentifier(CID);
     }
 
     @Override
     public int hashCode() {
         final int PRIME = 31;
         int result = 1;
-        result = PRIME * result + (int) (EID ^ (EID >>> 32));
+        result = PRIME * result + (int) (CID ^ (CID >>> 32));
         result = PRIME * result + (int) (AID ^ (AID >>> 32));
         return result;
     }
@@ -68,12 +68,12 @@ public class ActivityIdentifier implements Serializable {
 
         final ActivityIdentifier other = (ActivityIdentifier) obj;
 
-        return (EID == other.EID && AID == other.AID);
+        return (CID == other.CID && AID == other.AID);
     }
 
     public String toString() {
-        return "AID: " + Integer.toHexString((int) (EID >> 32) & 0xffffffff)
-                + ":" + Integer.toHexString((int) (EID & 0xffffffff)) + ":"
+        return "AID: " + Integer.toHexString((int) (CID >> 32) & 0xffffffff)
+                + ":" + Integer.toHexString((int) (CID & 0xffffffff)) + ":"
                 + Long.toHexString(AID); /*
                                           * + " (" + Integer.toHexString((int)(
                                           * lastKnownEID >> 32) & 0xffffffff) +
