d013537ca8166ef7f6a93c871dcbb69d40647110
Jason Maassen <jmaassen73@gmail.com>
2016-12-21 15:29:13 +0100

    Added Workflow test to integrationTest

---
+++ src/integrationTests/java/test/pipeline/inbalance/WorkflowTest.java
@@ -0,0 +1,73 @@
+package test.pipeline.inbalance;
+
+import java.util.Properties;
+
+import org.junit.Test;
+
+import ibis.constellation.Constellation;
+import ibis.constellation.ConstellationConfiguration;
+import ibis.constellation.ConstellationCreationException;
+import ibis.constellation.ConstellationFactory;
+import ibis.constellation.util.MultiEventCollector;
+import ibis.constellation.StealStrategy;
+import ibis.constellation.context.UnitActivityContext;
+import ibis.constellation.context.UnitExecutorContext;
+
+public class WorkflowTest {
+
+	private static int JOBS = 10;
+	private static int SIZE = 1024;
+	private static int RANK = 0;
+	
+	@Test
+	public void test() throws ConstellationCreationException {
+
+		// Simple test that creates, starts and stops a set of constellations.
+		// When the lot is running, it deploys a series of jobs.
+		int jobs = JOBS;
+		int size = SIZE;
+		int rank = RANK;
+
+		Properties p = new Properties();
+		p.put("ibis.constellation.distributed", "false");
+		
+		ConstellationConfiguration [] config = new ConstellationConfiguration[] { 
+				new ConstellationConfiguration(new UnitExecutorContext("MASTER"), StealStrategy.SMALLEST), 
+				new ConstellationConfiguration(new UnitExecutorContext("A"), StealStrategy.SMALLEST), 
+				new ConstellationConfiguration(new UnitExecutorContext("B"), StealStrategy.SMALLEST),
+				new ConstellationConfiguration(new UnitExecutorContext("X"), StealStrategy.SMALLEST),
+				new ConstellationConfiguration(new UnitExecutorContext("E"), StealStrategy.SMALLEST) };
+
+		Constellation c = ConstellationFactory.createConstellation(p, config);
+		c.activate();
+
+		int expected = (1600 * JOBS) + 300;
+		
+		if (rank == 0) {
+
+			long start = System.currentTimeMillis();
+
+			MultiEventCollector me = new MultiEventCollector(new UnitActivityContext("MASTER"), jobs);
+			c.submit(me);
+
+			for (int i = 0; i < jobs; i++) {
+
+				//System.out.println("SUBMIT " + i);
+
+				Data data = new Data(i, 0, new byte[size]);
+				c.submit(new Stage1(me.identifier(), 100, data));
+			}
+
+			//System.out.println("SUBMIT DONE");
+
+			me.waitForEvents();
+
+			long end = System.currentTimeMillis();
+
+			System.out.println("Total processing time: " + (end - start) + " ms. (expected = " + expected + " ms.)");
+		}
+
+		c.done();
+	}
+
+}
