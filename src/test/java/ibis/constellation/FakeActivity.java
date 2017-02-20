406a5d2d74ae92bcca6e97188dba9995df36568f
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2017-02-20 15:14:12 +0100

    Added timeout in pool master election; if there are no candidates after some time, become a candidate yourself

--- src/test/java/ibis/constellation/FakeActivity.java
+++ src/test/java/ibis/constellation/FakeActivity.java
@@ -16,10 +16,6 @@
 
 package ibis.constellation;
 
-import ibis.constellation.Activity;
-import ibis.constellation.Constellation;
-import ibis.constellation.Event;
-
 /**
  * @version 1.0
  * @since 1.0
@@ -28,18 +24,18 @@ import ibis.constellation.Event;
 public class FakeActivity extends Activity {
 
     private static final long serialVersionUID = -4021583343422065387L;
-   
-    boolean initialized = false;
-    boolean gotEvent = false;
-    boolean clean = false;
-    
+
+    protected boolean initialized = false;
+    protected boolean gotEvent = false;
+    protected boolean clean = false;
+
     /**
-     * 
+     *
      */
     public FakeActivity() {
         super(new Context("DEFAULT", 0, 0), true);
     }
-    
+
     @Override
     public int initialize(Constellation constellation) {
         initialized = true;
